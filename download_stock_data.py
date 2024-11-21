bb7e942e84c614a008df427c55221ddbe39139a7
Giovanni Donisi <82442402+gdonisi@users.noreply.github.com>
2024-11-21 12:56:02 +0100

    Clean data

--- download_stock_data.py
+++ download_stock_data.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 import yfinance as yf
 
 
@@ -7,9 +8,10 @@ def main():
     crypto = ["BTC-USD", "ETH-USD", "DOGE-USD", "ADA-USD", "SOL-USD"]
     forex = ["EURUSD=X", "GBPUSD=X", "JPY=X", "EURJPY=X", "EURGBP=X", "CNY=X"]
 
-    save_data(stocks, "stocks")
-    save_data(crypto, "crypto")
-    save_data(forex, "forex")
+    if not os.path.exists("data"):
+        save_data(stocks, "stocks")
+        save_data(crypto, "crypto")
+        save_data(forex, "forex")
 
 
 def save_data(data, name):
@@ -21,6 +23,14 @@ def save_data(data, name):
     for item in data:
         csv = yf.download(item, period="max")
         csv.to_csv(f"{folder}/{item.lower()}.csv")
+
+        # Clean data
+        df = pd.read_csv(f"{folder}/{item.lower()}.csv")
+        df.drop([0, 1], inplace=True)
+        df.rename(columns={"Price": "Date"}, inplace=True)
+        if name == "forex":
+            df.drop("Volume", axis=1, inplace=True)
+        df.to_csv(f"{folder}/{item.lower()}.csv", index=False)
 
 
 if __name__ == "__main__":
